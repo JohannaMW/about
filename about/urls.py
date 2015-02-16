949d2acfbbf07f1d03af1b9917464aca6b4f2dc4
Johanna <jweinsziehr@gmx.de>
2015-02-16 13:49:55 +0100

    setting up static files/bootstrap

--- about/urls.py
+++ about/urls.py
@@ -5,9 +5,7 @@ from django.contrib import admin
 from . import settings
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'about.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', 'me.views.home', name='home'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
