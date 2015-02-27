c40b3f737afd7abaf8de9dac1e7252ab7eb9b034
Johanna <jweinsziehr@gmx.de>
2015-02-27 14:39:29 +0100

    move js code to seperate file

--- about/static/js/about.js
+++ about/static/js/about.js
@@ -1,23 +1,30 @@
-/**
- * Created by johanna on 2/27/15.
- */
-var projects = {"1":"/static/img/dropbox.png", "2":"/static/img/mobie.png"};
+var projects =     {"1":["/static/img/dropbox.png", "this is the description"],
+                    "2":["/static/img/mobie.png", "this is the description of my second project"],
+                    "3":["/static/img/mobie.png", "this is the description of my third project"],
+                    "4":["/static/img/mobie.png", "this is the description of my fourth project"]
+                    };
 var source;
-var htmlString = "<img src=" + source + "><div><p>What's it about</p></div></img>";
+var description;
 
-$('#1').hover(function() {
-    console.log('in function');
+$('.project').mouseenter(function() {
     var id = (this.id);
     // get matching image source
     for (var key in projects) {
         if (key !== id) {
         } else {
-            source = projects[key];
-            console.log(source);
+            source = projects[key[0]];
+            description = source[1];
+            // remove color class
+            $(this).removeClass('blue light_green');
+            // add html
+            $(this).append("<img src=" + source[0] + "><div><p>" + String(description) + "</p></div></img>");
         }
     }
-    // remove color class
-    $(this).removeClass('blue light_green');
-    // add html
-    $(this).text(htmlString);
+});
+
+$('.project').mouseleave(function() {
+    // remove image tag
+    $(this).empty();
+    // add color class
+    $(this).addClass('blue');
 });
